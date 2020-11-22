56963480041dc6df7b8563ea7a3c48f195c1ed66
Florian PC <duhmeflorian@gmail.com>
2020-11-22 17:27:59 +0100

    Added loop for each KPI. Filter strings are now split by each KPI.

--- database_script.sql
+++ database_script.sql
@@ -1,21 +1,22 @@
 create or replace script example() as
     local success, res = pquery([[SELECT * FROM DB_SCRIPT."Infomover" ORDER BY "Kennzahl"]])
 
-    local case_when = ""
-    -- test
-    for i=1, #res do
-        if res[i][2] == "Lieferungen" then
-            local line = " CASE WHEN " .. res[i][3] .. "." .. res[i][4] .. " = " .. res[i][5] .. " THEN 'X' END AS " .. res[i][2]
-            print(line)
-        else
-            print(" CASE WHEN TEST = 'A' THEN 'X' END AS TEST")
-        end
-    end
+    local kpis = {"Lieferungen", "Transportaufträge"}
+    local filter_string = "CASE WHEN "
+    for k=1, #kpis do
+        for i=1, #res do
+            if res[i][2] == kpis[k] then
+                filter_string = filter_string .. res[i][3] .. "." .. res[i][4] .. " = " .. res[i][5] .. " AND "
 
-    local field = [[user_name,
-                   CASE WHEN user_name = 'SYS' THEN 'X' END AS Standarduser]]
-    local table = "EXA_ALL_SESSIONS"
-    res = query("CREATE OR REPLACE VIEW Test_View AS SELECT " .. field .. " FROM " .. table)
+            end
+        end
+        filter_string = filter_string .. "THEN 'X' END AS " .. kpis[k] .. " CASE WHEN "
+    end
+    print(filter_string)
+    -- local field = [[user_name,
+    --               CASE WHEN user_name = 'SYS' THEN 'X' END AS Standarduser]]
+    -- local table = "EXA_ALL_SESSIONS"
+    -- res = query("CREATE OR REPLACE VIEW Test_View AS SELECT " .. field .. " FROM " .. table)
 
 /
 execute script example() with output; 
